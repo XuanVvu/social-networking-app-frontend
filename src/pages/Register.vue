7a1c5ae39e6087fb8263d9ec66ce048fb3058ac4
XuanVu123 <nguyenxuanvu0307.it@gmail.com>
2024-08-02 13:39:28 +0700

    feat: make registere

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -11,18 +11,31 @@ import { RuleForm } from './Login.vue';
 const formSize = ref<ComponentSize>('default')
 
 const form = reactive({
+    firtName:'',
+    lastName:'',
     email: '',
     password: '',
-    rememberCheckbox: false
+    confirmPassword:'',
 })
 
 const ruleFormRef = ref<FormInstance>()
 const { navigateTo } = useNavigation()
-const onSubmit = () => {
+const onSubmit = async (formEl: FormInstance | undefined) => {
+    await formEl?.validate(async (valid) => { 
+    if (!valid) {
+        return;
+    }
     navigateTo('/login')
+})
 }
 
-const rules = reactive<FormRules<RuleForm>>({
+const rules = reactive<FormRules<RuleForm &{confirmPassword: string; firstName: string; lastName: string }>>({
+    firstName: [
+        { required: true, message: 'Vui lòng nhập firstName', trigger: 'blur' },
+    ],
+    lastName: [
+        { required: true, message: 'Vui lòng nhập lastName', trigger: 'blur' },
+    ],
     email: [
         { required: true, message: 'Vui lòng nhập email', trigger: 'blur' },
         { type: 'email', message: 'Vui lòng nhập đúng địa chỉ email', trigger: 'blur' }
@@ -33,6 +46,18 @@ const rules = reactive<FormRules<RuleForm>>({
             min: 6,
             message: 'Mật khẩu phải có ít nhất 6 kí tự',
             trigger: 'blur'
+        }
+    ],
+    confirmPassword: [
+        { required: true, message: 'Vui lòng xác nhận mật khẩu', trigger: 'blur' },
+        {
+           validator:(rule, value, callback) => {
+            if(value !== form.password) {
+                callback(new Error('Mật khẩu không khớp'))
+            } else {
+                callback()
+            }
+           }
         }
     ]
 })
@@ -51,15 +76,25 @@ const rules = reactive<FormRules<RuleForm>>({
 
                 <el-form class="flex flex-col gap-3" ref="ruleFormRef" :rules="rules" :model="form" :size="formSize"
                     status-icon>
+                    <el-form-item prop="firstName">
+                        <InputBase type="text" placehoder="First name" v-model="form.firtName" :icon="User" />
+                    </el-form-item>
+                    <el-form-item prop="firstName">
+                        <InputBase type="text" placehoder="Last name" v-model="form.lastName" :icon="User" />
+                    </el-form-item>
                     <el-form-item prop="email">
                         <InputBase type="email" placehoder="Email" v-model="form.email" :icon="User" />
                     </el-form-item>
                     <el-form-item prop="password">
                         <InputBase type="password" placehoder="Mật khẩu" v-model="form.password" :icon="Lock" />
                     </el-form-item>
+                    <el-form-item prop="confirmPassword">
+                        <InputBase type="password" placehoder="Nhập lại mật khẩu" v-model="form.confirmPassword" :icon="Lock" />
+                    </el-form-item>
+
 
                     <el-form-item>
-                        <Button classFromParent="w-full py-6 py-2" @click="onSubmit()">
+                        <Button classFromParent="w-full py-6 py-2" @click="onSubmit(ruleFormRef)">
                             Đăng ký
                         </Button>
                     </el-form-item>
