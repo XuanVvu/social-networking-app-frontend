a05fbdb7b12e3354511944272dee5a5d2bd8c7b3
XuanVu123 <nguyenxuanvu0307.it@gmail.com>
2024-11-08 10:13:38 +0700

    upadte i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,24 +1,13 @@
 import { createI18n } from 'vue-i18n'
-import messages from '@intlify/unplugin-vue-i18n/messages'
+import en from './locales/en.json'
+import vi from './locales/vi.json'
 
-function loadLocaleMessages() {
-  const locales = import.meta.glob('./locales/*.json', { eager: true })
-  const messages: { [key: string]: any } = {}
-
-  for (const path in locales) {
-    const matched = path.match(/([A-Za-z0-9-_]+)\.json$/i)
-    if (matched && matched.length > 1) {
-      const locale = matched[1]
-      messages[locale] = (locales[path] as any).default
-    }
-  }
-  return messages
-}
+const messages = { en, vi }
 
 export default createI18n({
   legacy: false,
   globalInjection: true,
-  locale: 'vi',
+  locale: 'en',
   fallbackFormat: 'en',
-  messages: loadLocaleMessages()
+  messages: messages
 })
