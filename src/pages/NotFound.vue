6890297a4dc3054b6afcb5486ed4d098bdb95cf2
XuanVu123 <nguyenxuanvu0307.it@gmail.com>
2024-11-19 16:16:25 +0700

    feat: fix any error

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Not found Page</div>
+  <div class="uppercase flex justify-center pt-[100px]">
+    <div class="flex flex-col items-center">
+      <p class=" text-md">oops! page not found</p>
+      <div class="text-9xl font-bold">404</div>
+      <p class=" text-xl">we are sorry, but the page you requested was not found</p>
+    </div>
+  </div>
 </template>
